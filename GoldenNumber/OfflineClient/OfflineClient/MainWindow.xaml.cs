8eeec1b56f93b0de531e1de3062585b2269ff5ac
Xu <chelxom@outlook.com>
2018-09-03 19:48:37 +0800

    Refine offine GNum client (#8)
    * refine rule, add comments
    
    * nit

--- GoldenNumber/OfflineClient/OfflineClient/MainWindow.xaml.cs
+++ GoldenNumber/OfflineClient/OfflineClient/MainWindow.xaml.cs
@@ -30,11 +30,22 @@ namespace OfflineClient
             Loaded += MainWindow_Loaded;
         }
 
+        private const char Delimiter = '\t';
+        private const string NewLine = "\n";
+
+        // !!! 需要提供实现的函数。
+        // 请注意，该函数会在后台线程执行。
+        private static Tuple<double, double> GetNumber(string input)
+        {
+            System.Threading.Thread.Sleep(40);
+            return Tuple.Create(42.0, 1.5d);
+        }
+
         private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             var lines = File.ReadAllLines(@"Data\SecondData.txt");
 
-            var meta = lines[0].Split('\t').Select(int.Parse).ToList();
+            var meta = lines[0].Split(Delimiter).Select(int.Parse).ToList();
             var totalRound = meta[0];
             var columnCount = meta[1];
 
@@ -48,58 +59,88 @@ namespace OfflineClient
             {
                 var br = await Task.Run(() =>
                 {
-                    var input = $"{i}\t{columnCount}\n" + string.Join("\n", lines.Skip(1).Take(i));
-                    var sw = Stopwatch.StartNew();
-
-                    var candidate = GetNumber(input);
-
-                    var elapsed = sw.Elapsed;
-
-                    return new BotResult
+                    var input = $"{i}{Delimiter}{columnCount}";
+                    if (i > 0)
                     {
-                        Candidate = candidate,
-                        ElapsedTime = elapsed,
-                    };
+                        // 避免没有数据时，末尾会有一个空行。否则可能影响解析。
+                        input += NewLine;
+                        input += string.Join(NewLine, lines.Skip(1).Take(i));
+                    }
+
+                    var botResult = new BotResult();
+                    var sw = Stopwatch.StartNew();
+                    try
+                    {
+                        var submittedNumbers = GetNumber(input);
+                        botResult.Candidate = submittedNumbers;
+                    }
+                    catch (Exception)
+                    {
+                    }
+
+                    botResult.ElapsedTime = sw.Elapsed;
+                    return botResult;
                 });
 
+                // 检查提交的数字是否有效。
+                var submitted = YieldFromTuple(br.Candidate).ToArray();
+                if (submitted != null)
+                {
+                    if (!submitted.All(IsValidNumber))
+                    {
+                        submitted = null;
+                    }
+                }
+
+                // submitted 变量不为 null，即确实提交了有效的数字。
+                if (submitted != null)
+                {
+                    // 最后一回合时，还需要取最后一行数据来计算结果，所以最后一行不能用作历史数据。
+                    var numbersThisRound = lines[i + 1].Split(Delimiter)
+                        .Skip(1) // 在计算当前轮的结果时，去掉历史数据里的黄金点，并重新计算。
+                        .Select(double.Parse).ToList();
+                    numbersThisRound.AddRange(submitted);
+                    var newG = numbersThisRound.Average() * 0.618;
+
+                    // 每个数据都有序号（从0开始），以便我们追踪。当前的参与者提交的数据，放在最后。
+                    int[] indexes = { numbersThisRound.Count - 1, numbersThisRound.Count - 2 };
+
+                    // 不只是进行简单地排序。
+                    // 由于可能有一样的数据，所以我们先按数据进行分组，然后再排序。
+                    var sortedGroup = numbersThisRound
+                        .Select((n, index) => new { Diff = Abs(n - newG), Index = index }) // 一种 Enumerable.Select 方法的重载，是同时提供了从0开始的序号的。
+                        .GroupBy(diffAndIndex => diffAndIndex.Diff)
+                        .OrderBy(group => group.Key) // group.Key 就是重复的那个数字。
+                        .ToList();
+
+                    // 由于我们对可能相同的数据做了分组，又记了序号，所以我们只需检查当前提交的数对应的序号，
+                    // 是否属于距离最小组，或距离最大组即可。
+                    var nearestIndexes = new HashSet<int>(sortedGroup.First().Select(di => di.Index));
+                    var farthestIndexes = new HashSet<int>(sortedGroup.Last().Select(di => di.Index));
+
+                    int thisRoundScore = 0;
+
+                    var submittedNotNearestIndexes = indexes.Where(idx => !nearestIndexes.Contains(idx)).ToArray();
+                    if (submittedNotNearestIndexes.Length < indexes.Length)
+                    {
+                        // 存在某个提交的数，属于距黄金点最近的一组。并且最多只得一次分。
+                        thisRoundScore += numbersThisRound.Count / 2 - 1; // 统计人数时不计当前玩家。
+                        nearestCount++;
+                    }
+
+                    if (submittedNotNearestIndexes.Any(farthestIndexes.Contains))
+                    {
+                        // 除了最近的数以外，还存在最远的数。并且最多只扣一次分。
+                        // 如果所有数都一样，从上面可知，算是得分。
+                        thisRoundScore -= 2;
+                        farthestCount++;
+                    }
+
+                    score += thisRoundScore;
+                }
+
                 sumTime += br.ElapsedTime;
-                maxTime = TimeSpan.FromTicks(Max(maxTime.Ticks, br.ElapsedTime.Ticks));
-
-                var submitted = br.Candidate;
-                var numbersThisRound = lines[i + 1].Split('\t')
-                    .Skip(1) // exclude old G
-                    .Select(double.Parse).ToList();
-                numbersThisRound.Add(submitted.Item1);
-                numbersThisRound.Add(submitted.Item2);
-
-                int participantIndex1 = numbersThisRound.Count - 1;
-                int participantIndex2 = numbersThisRound.Count - 2;
-
-                var newG = numbersThisRound.Average() * 0.618;
-
-                var sortedGroup = numbersThisRound
-                    .Select((n, index) => new { Diff = Abs(n - newG), Index = index })
-                    .GroupBy(diffAndIndex => diffAndIndex.Diff)
-                    .OrderBy(group => group.Key)
-                    .ToList();
-
-                var nearestIndexes = new HashSet<int>(sortedGroup.First().Select(di => di.Index));
-                var farthestIndexes = new HashSet<int>(sortedGroup.Last().Select(di => di.Index));
-                int thisRoundScore = 0;
-
-                if (nearestIndexes.Contains(participantIndex1) || nearestIndexes.Contains(participantIndex2))
-                {
-                    thisRoundScore += numbersThisRound.Count / 2;
-                    nearestCount++;
-                }
-
-                if (farthestIndexes.Contains(participantIndex1) || farthestIndexes.Contains(participantIndex2))
-                {
-                    thisRoundScore -= 2;
-                    farthestCount++;
-                }
-
-                score += thisRoundScore;
+                maxTime = maxTime < br.ElapsedTime ? br.ElapsedTime : maxTime;
 
                 NearestText.Text = $"{nearestCount}/{i + 1}";
                 FarthestText.Text = $"{farthestCount}/{i + 1}";
@@ -108,14 +149,18 @@ namespace OfflineClient
                 MaxTimeText.Text = $"{maxTime.TotalMilliseconds}ms";
 
                 ScoreText.Foreground = score < 0 ? new SolidColorBrush(Colors.Red) : new SolidColorBrush(Colors.Green);
-
             }
         }
 
-        private static Tuple<double, double> GetNumber(string input)
+        private static bool IsValidNumber(double num)
         {
-            System.Threading.Thread.Sleep(40);
-            return Tuple.Create(42.0, 1.5d);
+            return 0 < num && num < 100;
+        }
+
+        private static IEnumerable<double> YieldFromTuple(Tuple<double, double> t)
+        {
+            yield return t.Item1;
+            yield return t.Item2;
         }
     }
 
